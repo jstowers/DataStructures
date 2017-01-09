815f9ce264018097a93b3f15ac3de1ccf031a331
Joe Stowers <greenchiletexan@gmail.com>
2017-01-09 01:23:18 -0700

    Complete push and pop functionality for stack data structure implemented with linked list

--- StackWithLinkedList.js
+++ StackWithLinkedList.js
@@ -1,28 +1,103 @@
 // Stack with Linked List Implementation
-// January 8, 2019
+// January 8, 2017
+
+//  Stack API
+/*
+	LIFO = Last In, First Out
+
+	1.	Create an empty stack
+
+	2.	Push(value) => insert a new value onto the stack
+
+	3.	Pop() => remove and return most recently added value
+
+	4.	isEmpty() => returns T/F if stack empty
+*/
+
+
+let Node =  function(value) {
+	return {
+		value: value,
+		next: null
+	}
+}
+
+let LinkedList = function(initialValue) {
+
+	if (initialValue) {
+		this.head = Node(initialValue);
+	} else {
+		this.head = Node(null);
+	}
+};
+
+LinkedList.prototype.push = function(value) {
+
+	// save a link to the old head
+	let oldhead = this.head;
+
+	// create a new head with the current value
+	this.head = new Node(value);
+
+	// assign oldhead to next
+	this.head.next = oldhead;
+
+};
+
+LinkedList.prototype.pop = function() {
+
+	let item = this.head.value;
+
+	this.head = this.head.next;
+
+	return item;
+
+};
+
+LinkedList.prototype.isEmpty = function() {
+
+
+
+};
+
+
 
 // Require file system module
 let fs = require('fs');
 
-// Synchronous call to read stringFile
+// Synchronous call to read string file
 let string = fs.readFileSync(process.argv[2], 'utf-8');
 
-// 
+// Split string into array
+let strToArr = string.split(" ");
 
+// Create a stack with linked list implementation
+let stack = new LinkedList();
 
-console.log('strToArr =', string.split());
-console.log('string.length =', string.length);
+// Loop through string array and test stack functionality
+while( strToArr.length > 0 ) {
 
-
-while( string.length > 0 ) {
-
+	// test stack functionality
 	// if string[0] = '-', pop string from stack and print
-
+	if (strToArr[0] === '-') {
+		let output = stack.pop();
+		console.log('output =', output);
+	}
 
 	// otherwise, push string[0] onto stack
-
+	else {
+		stack.push(strToArr[0]);
+	}
 
 	// string.slice()
-
+	strToArr = strToArr.slice(1)
 
 }
+
+
+
+
+
+
+
+
